3632bca6f63c293ed114ad0567a694eaa87da7ce
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-01-07 15:13:12 +0100

    ok

--- noteApp/public/js/main.js
+++ noteApp/public/js/main.js
@@ -1,3 +1,16 @@
 registerBtn.addEventListener("click",()=>{
     //alert("works")
+    if(!loginInput.value || !pwdInput.value)
+        return alert("please fill all the fields")
+    fetch("http://localhost:3000/register",{
+        method:"POST",
+        headers :{
+            "Content-Type":"application/json"
+        }
+        ,
+        body:JSON.stringify({
+            login:loginInput.value,
+            pwd: pwdInput.value
+        })
+    })
 })
