de10334d821647f618a78a17378ac0af7762d11a
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2025-01-07 15:34:46 +0100

    ok

--- noteApp/app.js
+++ noteApp/app.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const PORT = 3000
 
 const app = express()
+app.use(bodyParser.json())
 app.use(express.static("./public"))
+app.post("/register",(req,res)=>{
+    console.log(req.body)
+    
+})
 app.listen(PORT,(err)=>{
     if(err)
         return console.log("erreur",err)
